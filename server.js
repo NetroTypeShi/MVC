ac59ae49608cdfe02e90b2cb1cd888d1ea996b46
cojonesandrew@gmail.com <carti@opium.myguest.virtualbox.org>
2025-04-09 11:42:03 +0200

    yeah

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const {getAllUsers, addUser} = require("./models/users");
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static('public'));
